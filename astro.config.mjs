d44ee13ec9a7116d04d590a366971466f9400be4
Christian Gagliardi Pengu <pengu@goinfoteam.it>
2024-08-23 17:39:04 +0200

    sitemap + fix email

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,22 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
- 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
+  site: 'https://www.christianpengu.com',
+  integrations: [
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+    }),
+  ],
   output: 'server',
   adapter: vercel({
     webAnalytics: {
+      enabled: true,
+    },
+    speedInsights: {
       enabled: true,
     },
   }),
